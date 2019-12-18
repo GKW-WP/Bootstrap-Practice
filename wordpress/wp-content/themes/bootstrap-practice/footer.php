c53b45c5848821daef61360f8895777df60636f5
Cruz Ramirez <cruz_ramirez@ymail.com>
2019-12-18 14:12:45 -0800

    Fixed functions.php and file structure

---
+++ wordpress/wp-content/themes/bootstrap-practice/footer.php
@@ -0,0 +1,8 @@
+		<footer>
+			<h5>
+				I am a footer
+			</h5>
+		</footer>
+		<?php wp_footer();?>
+	</body>
+</html>
--- wordpress/wp-content/themes/bootstrap-practice/index.php
+++ wordpress/wp-content/themes/bootstrap-practice/index.php
@@ -1,2 +1,4 @@
-<h1>This is your theme to practice bs4.</h1>
-<h2>by Cruz Ramirez</h2>
+<?php get_header();?>
+	<h1>This is your theme to practice bs4.</h1>
+	<h2>by CRamirez</h2>
+<?php get_footer();?>
